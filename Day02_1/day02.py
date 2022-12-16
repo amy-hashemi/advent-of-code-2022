04265341926e292c147831189d8e5f808efbd8e2
Amy Hashemi <amy.hashemi@abcellera.com>
2022-12-15 16:11:58 -0800

    day 2 part 1 implementation

---
+++ Day02_1/day02.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+import pandas as pd
+import csv
+
+cwd = os.chdir("c:\\Users\\AmyHashemi\\Python\\advent-of-code-2022\\Day02_1")
+
+with open("input.txt", "r") as file:
+    data = csv.reader(file, delimiter = " ")
+    games = [tuple(line) for line in data]
+    #games = [tuple(line.split()) for line in data]
+
+for line in games:
+    print(line)
+
+
+#initialization
+score = 0
+draw = 3
+win = 6
+
+responseVal = {"X": 1, "Y": 2, "Z": 3}
+victoryScores = [('A', 'Y'), ('B', 'Z'), ('C', 'X')]
+drawScores = [('A', 'X'), ('B', 'Y'), ('C', 'Z')]
+
+for item in games:
+    score += responseVal[item[1]]
+    if item in victoryScores:
+        score += win
+    if item in drawScores:
+        score += draw
+
+print(score)
+    
+
+
+
+
+
