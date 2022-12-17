fcd9fc4658523470386a8302b665de876a61ab99
Amy Hashemi <amy.hashemi@abcellera.com>
2022-12-16 16:13:32 -0800

    completed part 1

--- Day03/day03-p1.py
+++ Day03/day03-p1.py
@@ -1,17 +1,6 @@
 import os
 import string
 
-# strings need to be split in half to get first and second items
-# lowercase a through z have priorities 1-26
-# uppercase A through Z have priorities 27-52
-# sum of priorities in a single item equals the total priority of the item
-# figure out which items appear in both compartments
-
-# find the item type that appears in both compartments of each rucksack -> find repeat characters in each item string, 
-# and sum the priorities of all of those items
-
-# build dict of characters and set values from 0 to 1 if they're found. Then check that hashmap against the other string. if it changes from 1 to 0, then it's a duplicate
-
 cwd = os.chdir("c:\\Users\\AmyHashemi\\Python\\advent-of-code-2022\\Day03")
 data = open("input.txt", "r")
 
@@ -34,28 +23,12 @@ for line in data:
     rucksackPair = (firstItem, secondItem)
     rucksackItems.append(rucksackPair)
 
-
-#test code
-""" rucksackItems = []
-line = "twRCpZVjVWqVSqVwwjtZfrrfntfvznBssBncfLrc"
-doubleItem = line.rstrip()
-firstItem = doubleItem[:len(doubleItem)//2]
-secondItem = doubleItem[len(doubleItem)//2:]
-rucksackPair = (firstItem, secondItem)
-rucksackItems.append(rucksackPair) """
-
-
 for collection1, collection2 in rucksackItems:
     dict_complete = {x:0 for x in dict_complete}
     for item1 in collection1:
-        itemCount += 1
-        if itemCount != len(collection2):
-            if dict_complete[item1] == 0:
-                dict_complete[item1] = 1
+        if dict_complete[item1] == 0:
+            dict_complete[item1] = 1
     for item2 in collection2:
-        print(collection1)
-        print(collection2)
-        print(item2)
         if dict_complete[item2] == 1:
             print("found a duplicate character between the two collections! The character is: ", item2)
             if item2.islower():
